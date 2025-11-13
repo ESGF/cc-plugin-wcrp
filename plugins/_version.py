15549317158bfbac6577cdda8a0575ad241f2146
RubaGarcia <rgm177@alumnos.unican.es>
2025-11-13 13:28:01 +0100

    plugin version update

--- plugins/_version.py
+++ plugins/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.1.dev29'
-__version_tuple__ = version_tuple = (0, 1, 1, 'dev29')
+__version__ = version = '0.1.1.dev30'
+__version_tuple__ = version_tuple = (0, 1, 1, 'dev30')
 
-__commit_id__ = commit_id = 'gb3c3d6b06'
+__commit_id__ = commit_id = 'g80dda1f59'
